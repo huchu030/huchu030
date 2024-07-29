5a5968747abe6ba973fcf904bf38132146aa8ac0
huchu030 <elleonoel31@naver.com>
2024-07-30 05:40:02 +0900

    토키봇업로드

---
+++ tokibot.py
@@ -0,0 +1,22 @@
+{
+  "name": "tokibot",
+  "version": "1.0.0",
+  "description": "tokibot",
+  "main": "index.js",
+  "scripts": {
+    "test": "hi"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/huchu030/huchu030.git"
+  },
+  "keywords": [
+    "toki"
+  ],
+  "author": "huchu030",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/huchu030/huchu030/issues"
+  },
+  "homepage": "https://github.com/huchu030/huchu030#readme"
+}
