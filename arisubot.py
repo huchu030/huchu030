0e3ab924b44d257ab9eb9d0e60171aed410f4443
huchu030 <elleonoel31@naver.com>
2024-08-01 11:40:06 +0900

    d

--- arisubot.py
+++ arisubot.py
@@ -141,6 +141,10 @@ async def 로봇주제에(interaction: discord.Interaction):
 async def 밥(interaction: discord.Interaction):
     await interaction.response.send_message("응..? 아리스는 건전지를 먹지 않습니다!", ephemeral=False)
 
+@bot.tree.command(name='쓰담', description="아리스의 인공 단백질 피부가 따뜻해집니다")
+async def 쓰담(interaction: discord.Interaction):
+    await interaction.response.send_message("아리스는 행복합니다..", ephemeral=False)
+
 @bot.tree.command(name='숫자야구_규칙', description="아리스가 숫자야구의 규칙을 설명해줍니다")
 async def 숫자야구_규칙(interaction: discord.Interaction):
     await interaction.response.send_message("[숫자야구 룰]\n \n아리스가 정한 3자리의 숫자를 맞히는 게임입니다!\n사용되는 숫자는 0부터 9까지 서로 다른 숫자 3개이며\n숫자와 위치가 전부 맞으면 S (스트라이크),\n숫자는 맞지만 위치가 틀렸을 경우 B (볼) 입니다.\n \n예시를 들어볼까요? 제가 정한 숫자가 ‘123’이면\n456 : 0S0B\n781 : 0S1B\n130 : 1S1B\n132 : 1S2B\n123 : 3S0B 입니다!\n \n아리스랑 같이 놀아요 끄앙", ephemeral=False)
@@ -149,7 +153,7 @@ async def 숫자야구_규칙(interaction: discord.Interaction):
 async def lotto(interaction: discord.Interaction):
     numbers = random.sample(range(1, 46), 6)
     numbers.sort()
-    await interaction.response.send_message(f'이번 주 로또 번호는~ {numbers} 입니다.\n꼭 당첨되길 바라요!', ephemeral=False)
+    await interaction.response.send_message(f'이번 주 로또 번호는~ {numbers} 입니다!\n당첨되면 저도...', ephemeral=False)
 
 
 
