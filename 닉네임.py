79a683cd737c3bac2da41d5920f8c6bae8904cd3
huchu030 <elleonoel31@naver.com>
2024-08-05 03:22:35 +0900

    d

--- 닉네임.py
+++ 닉네임.py
@@ -9,3 +9,7 @@ guild = interaction.guild
     user_nickname = get_user_nickname(guild, interaction.user.id)
 
 {user_nickname}
+
+data = self.load_game_data()
+        if user_id in data["players"]:
+            if user_id not in data["players"]:
