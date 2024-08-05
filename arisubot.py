62342763d70ba5430c020dd1b2951df5e2b995c2
huchu030 <elleonoel31@naver.com>
2024-08-06 06:53:07 +0900

    d

--- arisubot.py
+++ arisubot.py
@@ -379,7 +379,7 @@ class rpg:
             try:
                 await interaction.response.send_message(f"[{user_nickname}님의 스탯] \n"
                                                         f"\n레벨 : {player_data['level']}, 체력 : {player_data['hp']}, 경험치 : {player_data['exp']}\n"
-                                                        f"공격력 : {player_data.get['attack', 0]}, 방어력 : {player_data['defense']}\n"
+                                                        f"공격력 : {player_data['attack']}, 방어력 : {player_data['defense']}\n"
                                                         f"회피 확률 : {player_data['evasion_chance']}%, 공격 성공 확률 : + {player_data['attack_chance']}%p\n"
                                                         f"크리티컬 확률 : {player_data['critical_chance']}%, 크리티컬 데미지 : {player_data['critical_damage']*100}%\n"
                                                         f"수학의 정석 : {player_data['evasion_items']}개\n"
