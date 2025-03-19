5dd0b241ccda9d16193780c7c7e8b956701431e7
huchu030 <elleonoel31@naver.com>
2025-03-19 01:37:44 +0000

    dd

--- tokibot.py
+++ tokibot.py
@@ -139,6 +139,7 @@ class ThirtyOne:
         if not game.game_active:
             await interaction.response.send_message("게임이 진행중이지 않음")
         else:
+            result = await game.make_add(add, interaction)
             await interaction.response.send_message(result)
 
     async def give_up(self, interaction: discord.Interaction):
